12b6c21b66b6e77f210283aa7d3f120edbf833fa
Koi <58238284+adm1nSQL@users.noreply.github.com>
2023-02-03 21:12:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import telebot
 from time import sleep
 from loguru import logger
 from bot import loader
+
 # 1.22增加了日志功能，记录用户使用的指令和获取的订阅日志
 logger.add('bot.log')
-# 定义bot管理员的telegram userid ['1881396047']
+
+# 定义bot管理员的telegram userid ['xxxxxxxxx']
 admin_id = []
+
 # 定义bot
 bot_token = ''
+
 if __name__ == '__main__':
     # 命令行参数处理
     if len(sys.argv) == 1:
@@ -45,11 +49,12 @@ if __name__ == '__main__':
         logger.warning("无管理员或bot_token,请检查输入的参数")
         exit()
     print('=====程序已启动=====')
-    while True:
-        try:
+    try:
+        while True:
             loader(bot, admin_id=admin_id)
             bot.polling(none_stop=True)
-        except KeyboardInterrupt:
-            break
-        except Exception as e:
-            sleep(30)
+    except KeyboardInterrupt:
+        exit()
+    except Exception as e:
+        print(e)
+        sleep(30)
