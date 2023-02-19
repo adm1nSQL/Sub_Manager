93d091d0eabfe8b5c9ef6601f27c1b214f62909b
Koi <58238284+adm1nSQL@users.noreply.github.com>
2023-02-19 20:57:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from bot import loader
 logger.add('bot.log')
 
 # 定义bot管理员的telegram userid ['xxxxxxxxx']
+super_admin = ''
 admin_id = []
 
 # 定义bot
@@ -21,37 +22,44 @@ if __name__ == '__main__':
         try:
             with open('./config.json', 'r', encoding='utf-8') as fp:
                 config = json.load(fp)
+                super_admin = config.get('super_admin', '')
                 admin_id = config.get('admin', [])
                 bot_token = config.get('token', '')
+                print(super_admin + admin_id + bot_token)
         except FileNotFoundError:
-            print('Usage:\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, --token \tTelegram机器人的bot_token')
+            print('Usage:\n -s, --super_admin \t超级管理员id\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, '
+                  '--token \tTelegram机器人的bot_token')
             exit()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ha:t:", ["admin=", "token="])
+        opts, args = getopt.getopt(sys.argv[1:], "hs:a:t:", ["super_admin=", "admin=", "token="])
     except getopt.GetoptError:
-        print('Usage:\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, --token \tTelegram机器人的bot_token')
+        print('Usage:\n -s, --super_admin \t超级管理员id\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, '
+              '--token \tTelegram机器人的bot_token')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('Usage(使用帮助):\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, --token \tTelegram机器人的bot_token')
+            print('Usage(使用帮助):\n -s, --super_admin \t超级管理员id\n -a, --admin \t管理员名单id,多个id之间以英文逗号(,)间隔\n -t, '
+                  '--token \tTelegram机器人的bot_token')
             sys.exit()
+        elif opt in ("-s", "--super_admin"):
+            super_admin = arg
         elif opt in ("-a", "--admin"):
             admin_id = arg.split(',')
         elif opt in ("-t", "--token"):
             bot_token = arg
-    logger.info(f"管理员名单加载: {str(admin_id)}")
+    logger.info(f"超级管理员名单加载: {super_admin}，管理员名单加载: {str(admin_id)}")
     with open('./config.json', 'w', encoding='utf-8') as fp:
-        json.dump({'admin': admin_id, 'token': bot_token}, fp)
-    if bot_token and admin_id:
+        json.dump({'super_admin': super_admin, 'admin': admin_id, 'token': bot_token}, fp)
+    if bot_token and super_admin and admin_id:
         bot = telebot.TeleBot(bot_token)
     else:
         bot = None
-        logger.warning("无管理员或bot_token,请检查输入的参数")
+        logger.warning("无超级管理员或管理员或bot_token,请检查输入的参数")
         exit()
     print('=====程序已启动=====')
     try:
         while True:
-            loader(bot, admin_id=admin_id)
+            loader(bot, admin_id=admin_id, super_admin=super_admin)
             bot.polling(none_stop=True)
     except KeyboardInterrupt:
         exit()
